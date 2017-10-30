a2ecd9c3cbeeea4050add534f3edc8a9257ded34
Steffen Park <dev@istherepie.com>
2017-10-30 14:22:57 +0100

    Import vuejs boilerplate

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5>{{ message }}</h5>
+    <p>{{ submessage }}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: 'Hello Frank',
+      submessage: 'This is a submessage'
+    }   
+  },
+  methods: {
+    writeToConsole() {
+      console.log('Writing something in the log')
+    }
+  },
+  created() {
+    this.writeToConsole()
+  }
+}
+</script>
